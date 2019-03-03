47d681558a25df2c7540ca1a09bb0548a70136ae
wip-abramson <wip.abramson@gmail.com>
2019-03-03 15:44:47 +0000

    working editor

--- src/extensions/ace-editor.js
+++ src/extensions/ace-editor.js
@@ -1,9 +1,45 @@
 const showdown  = require('showdown');
 
+let satyrnicon = {
+  editors:{},
+  state:{}
+};
+
+function addEditor(key) {
+  let editor = ace.edit(key);
+  editor.setTheme("ace/theme/twilight");
+  editor.session.setMode("ace/mode/javascript");
+  satyrnicon.editors[key]=editor
+  satyrnicon.state[key]=editor.getValue()
+}
+function getEditor(key) {
+  return satyrnicon.editors[key]
+}
+
+function reset(key) {
+  let editor = getEditor(key)
+  editor.setValue(satyrnicon.state[key])
+}
+
+function run(key) {
+  let editor = getEditor(key);
+  console.oldLog = console.log;
+  console.log = function(value)
+  {
+    console.oldLog(value);
+    return value;
+  };
+
+  const code = editor.getValue()
+  const output = eval(code)
+  document.querySelector("#output-"+key).innerHTML = output
+  console.log = console.oldLog
+}
+
 function getEditorHtml(content, key) {
-  return "<div id=\"showdown-js-editor\">\n" +
-    "    <input type=\"button\" onclick=\"run("+key+")\" value=\"Run\" />\n" +
-    "    <input type=\"button\" onclick=\"reset("+key+")\" value=\"Refresh\" />\n" +
+  return "<div class=\"showdown-js-editor\">\n" +
+    "    <input type=\"button\" onclick=\"run('"+key+"')\" value=\"Run\" />\n" +
+    "    <input type=\"button\" onclick=\"reset('"+key+"')\" value=\"Refresh\" />\n" +
     "\n" +
     "    <pre id=\""+key+"\" class=\"editor\">" + content +
     "    </pre>\n" +
